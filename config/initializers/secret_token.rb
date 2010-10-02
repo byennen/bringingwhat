ea3f338df07aa91b85adcf3e091699083dba474b
Lance Ennen <byennen@gmail.com>
2010-10-02 01:55:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BringingwhatCom::Application.config.secret_token = 'cad95967f5b72533cbeb6a5e9c18fbc73258791bf1d60039ea814c22b61bc2abead0e2726dfdf8d36daf274c0ba70ce9a226ba7f8e9161bd80b9051ef13af928'
