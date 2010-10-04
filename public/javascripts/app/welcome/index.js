1553c6e8069626f7085994b19f1f177e7f9b0f8b
Lance Ennen <byennen@gmail.com>
2010-10-04 01:30:03 -0500

    added fancy box

--- public/javascripts/app/welcome/index.js
+++ public/javascripts/app/welcome/index.js
@@ -1,1 +1,12 @@
-// hello
+$('#welcome a').click(function() {
+	$.fancybox({
+      'width': '50%',
+      'height': '50%',
+			'hideOnOverlayClick': false,
+      'autoScale': true,
+      'transitionIn': 'elastic',
+      'transitionOut': 'elastic',
+      'type': 'iframe',
+      'content': 'http://facebook.com'
+  });
+});
