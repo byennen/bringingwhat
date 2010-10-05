6d8b3a43786c0e5113230d791865cc855127c995
Lance Ennen <byennen@gmail.com>
2010-10-04 23:21:28 -0500

    adding users login

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_filter :require_user
+  
   # GET /events
   # GET /events.xml
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 BringingwhatCom::Application.routes.draw do
   
+  get "user_sessions/new"
+
+  #dashboard
   get "dashboard/index"
 
   resources :events
+  resources :user_sessions
 
+  #login
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+  
+  #facebook
   get "authorize" => 'authorize#start'
   get "auth_callback" => 'authorize#callback'
   
+  #welcome page
   root :to => "welcome#index"
 
 end
